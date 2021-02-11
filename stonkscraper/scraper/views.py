e16f27126a599f878d50ba1c4badd5be0bfcea55
Jackie <taekwondo.chan@gmail.com>
2021-02-10 17:48:37 -0800

    update routes to support new get_one

--- stonkscraper/scraper/views.py
+++ stonkscraper/scraper/views.py
@@ -1,9 +1,37 @@
 from scraper import app
 from flask import render_template, request, send_file
-
+from .scraper import Scraper
 
 @app.route('/')
 def home():
     """Displays the homepage with forms for current or historical data."""
 
     return "hello"
+
+@app.route('/<ticker>')
+def get_all(ticker):
+    stock = Scraper(ticker)
+
+    return stock.get_all()
+
+
+@app.route('/<ticker>/name')
+def get_name(ticker):
+    stock = Scraper(ticker)
+    stock.scrape_page()
+
+    if stock.page_content == False:
+        return 'ERROR'
+
+    return stock.get_one('name')
+
+@app.route('/<ticker>/current')
+def get_current(ticker):
+    stock = Scraper(ticker)
+    stock.scrape_page()
+
+    if stock.page_content == False:
+        return 'ERROR'
+
+    # Restructure for timestamp/name comparisons
+    return stock.get_one('current')
